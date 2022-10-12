0366c1e7610ab71953bb59962abc9f888237f895
prashantsanjeevpandey <p4prashantp4@gmail.com>
2022-10-13 01:58:58 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,53 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'POCs';
 
+  payRequest = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [
+      {
+        type: 'CARD',
+        parameters: {
+          allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+          allowedCardNetworks: ['AMEX', 'VISA', 'MASTERCARD'],
+        },
+        tokenizationSpecification: {
+          type: 'PAYMENT_GATEWAY',
+          parameters: {
+            gateway: 'mpgs',
+            gatewayMerchantId: 'TESTMCDONATION02',
+          },
+        },
+      },
+    ],
+    merchantInfo: {
+      merchantId: '12345678901234567890',
+      merchantName: 'Demo Merchant',
+    },
+    transactionInfo: {
+      totalPriceStatus: 'FINAL',
+      totalPriceLabel: 'Total',
+      totalPrice: '100.00',
+      currencyCode: 'USD',
+      countryCode: 'US',
+    },
+  };
+
   onLoadPaymentData = (event: any): void => {
-    console.log('load payment data', event.detail);
+    const tokenData = JSON.parse(
+      event.detail.paymentMethodData.tokenizationData.token
+    );
+    const signedMessage = JSON.parse(tokenData.signedMessage);
+    tokenData.signedMessage = signedMessage;
+    console.log('load payment data', tokenData);
+    console.log(JSON.stringify(tokenData));
   };
 
   onClick = (event: any): void => {
